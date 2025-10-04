b74cde56b866cf958a7fbdae0a566a91e9b2638a
Antoni <antoni.navajas7@gmail.com>
2025-10-04 13:26:33 +0200

    Ultimo digito programa yaaaaaaa el primero de todosh

---
+++ UltimDigit.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class UltimDigit{
+	public static void main(String[] args){
+		int num;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Escriu un numero");
+		num = scan.nextInt();
+		System.out.println("L'ultima xifra del numero es  " + num % 10);
+	
+	}
+
+
+
+}	
